24fecc00dd99183ed69effafafd0a0bc51407070
arimbawadx <yogade9595.yd@gmail.com>
2021-10-26 16:34:51 +0800

    new change

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,6 +17,7 @@
         <style type="text/css">
         div#preloader { position: fixed; left: 0; top: 0; z-index: 999; width: 100%; height: 100%; overflow: visible; background: #ffffff url('https://manifest.arimbawadx.com/loader/loader2.gif') no-repeat center center; }
     </style>
+    <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="ddc66e05-ee6f-4209-9654-091544cd514b";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
 </head>
 <body class="bg-dark">
     <div id="preloader"></div>
